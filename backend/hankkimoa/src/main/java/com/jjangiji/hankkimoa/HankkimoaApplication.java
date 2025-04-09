9ecf2657c75ff00a43f65dc587120c598f805286
jinwoo22 <jenny010925@naver.com>
2025-04-10 00:40:09 +0900

    chore: gitignore 설정

---
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/HankkimoaApplication.java
@@ -0,0 +1,13 @@
+package com.jjangiji.hankkimoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankkimoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HankkimoaApplication.class, args);
+	}
+
+}
---
+++ backend/hankkimoa/src/test/java/com/jjangiji/hankkimoa/HankkimoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjangiji.hankkimoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankkimoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

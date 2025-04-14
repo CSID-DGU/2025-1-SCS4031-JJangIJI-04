5b2a364450cd590e52f443c9825fe881165ad5c6
jinwoo22 <jenny010925@naver.com>
2025-04-15 03:17:19 +0900

    feat: RestaurantEntity 엔티티 생성

---
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/place/repository/RestaurantEntity.java
@@ -0,0 +1,30 @@
+package com.jjangiji.hankkimoa.place.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class RestaurantEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // todo 다른 필드 매핑
+
+    @Column(nullable = false)
+    private String name;
+
+    public RestaurantEntity(String name) {
+        this.name = name;
+    }
+}
---
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/place/repository/RestaurantRepository.java
@@ -0,0 +1,6 @@
+package com.jjangiji.hankkimoa.place.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
+}

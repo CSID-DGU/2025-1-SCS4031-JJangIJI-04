24e1214dd4d02896b1561eedcc88feedd312c791
jinwoo22 <jenny010925@naver.com>
2025-04-22 02:57:18 +0900

    feat: 지출 응답 기능 컨트롤러 구현

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/controller/ExpenseController.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/controller/ExpenseController.java
@@ -1,14 +1,18 @@
 package com.jjangiji.hankkimoa.expense.controller;
 
 import com.jjangiji.hankkimoa.expense.service.ExpenseService;
+import com.jjangiji.hankkimoa.expense.service.dto.DateExpenseResponse;
 import com.jjangiji.hankkimoa.expense.service.dto.ExpenseCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,6 +24,15 @@ public class ExpenseController {
     public ResponseEntity<Void> createExpense(@RequestBody ExpenseCreateRequest request) {
         Long expenseId = expenseService.createExpense(request);
         return ResponseEntity.created(URI.create("/expenses/" + expenseId)).build();
+    }
+
+    @GetMapping("/saving-goals/{savingGoalId}/expenses")
+    public ResponseEntity<DateExpenseResponse> getDailyExpenses(
+            @PathVariable Long savingGoalId,
+            @RequestParam LocalDate date) {
+        DateExpenseResponse dateExpenseResponse = expenseService.readDateExpenses(savingGoalId, date);
+        return ResponseEntity.ok()
+                .body(dateExpenseResponse);
     }
 
     @PostMapping("/expenses/{expenseId}")

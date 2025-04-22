9e33ed6134ab92c1f2ac95c0033fe25640f01d7d
jinwoo22 <jenny010925@naver.com>
2025-04-22 21:04:01 +0900

    feat: 지출 내역 월별 조회 컨트롤러 기능 구현

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/controller/ExpenseController.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package com.jjangiji.hankkimoa.expense.controller;
 import com.jjangiji.hankkimoa.expense.service.ExpenseService;
 import com.jjangiji.hankkimoa.expense.service.dto.DateExpenseResponse;
 import com.jjangiji.hankkimoa.expense.service.dto.ExpenseCreateRequest;
+import com.jjangiji.hankkimoa.expense.service.dto.MonthExpenseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,22 +21,29 @@ public class ExpenseController {
 
     private final ExpenseService expenseService;
 
-    @PostMapping("/expenses")
+    @PostMapping("/api/expenses")
     public ResponseEntity<Void> createExpense(@RequestBody ExpenseCreateRequest request) {
         Long expenseId = expenseService.createExpense(request);
         return ResponseEntity.created(URI.create("/expenses/" + expenseId)).build();
     }
 
-    @GetMapping("/saving-goals/{savingGoalId}/expenses")
-    public ResponseEntity<DateExpenseResponse> getDailyExpenses(
+    @GetMapping("/api/expenses")
+    public ResponseEntity<MonthExpenseResponse> readMonthExpenses( // TODO 타유저 정보도 볼 수 있게
+            @RequestParam("from") LocalDate from,
+            @RequestParam("to") LocalDate to) {
+        MonthExpenseResponse monthExpenseResponse = expenseService.readMonthExpenses(from, to);
+        return ResponseEntity.ok(monthExpenseResponse);
+    }
+
+    @GetMapping("/api/saving-goals/{savingGoalId}/expenses")
+    public ResponseEntity<DateExpenseResponse> readDateExpenses(
             @PathVariable Long savingGoalId,
             @RequestParam LocalDate date) {
         DateExpenseResponse dateExpenseResponse = expenseService.readDateExpenses(savingGoalId, date);
-        return ResponseEntity.ok()
-                .body(dateExpenseResponse);
+        return ResponseEntity.ok(dateExpenseResponse);
     }
 
-    @PostMapping("/expenses/{expenseId}")
+    @PostMapping("/api/expenses/{expenseId}")
     public ResponseEntity<Void> deleteExpense(@PathVariable("expenseId") Long expenseId) {
         expenseService.deleteExpense(expenseId);
         return ResponseEntity.noContent().build();

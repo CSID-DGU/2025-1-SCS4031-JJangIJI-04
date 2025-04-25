ad415dc451ff48c3f675187be31ce1308dae0dd0
jinwoo22 <jenny010925@naver.com>
2025-04-25 18:30:40 +0900

    feat: ExpenseSavingGoal 도메인과 연관관계 끊기

--- backend/hankkimoa/src/test/java/com/jjangiji/hankkimoa/expense/domain/ExpenseStatusTest.java
+++ backend/hankkimoa/src/test/java/com/jjangiji/hankkimoa/expense/domain/ExpenseStatusTest.java
@@ -1,25 +1,16 @@
 package com.jjangiji.hankkimoa.expense.domain;
 
-import com.jjangiji.hankkimoa.restaurant.domain.Restaurant;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
 
 class ExpenseStatusTest {
-
-    private final LocalDate startDate = LocalDate.of(2025, 4, 1);
-    private final ExpenseSavingGoal expenseSavingGoal = new ExpenseSavingGoal(1L,
-            100_000,
-            startDate,
-            LocalDate.of(2025, 4, 10));
-    private final Restaurant restaurant = new Restaurant(1L, "한끼식당");
 
     @DisplayName("데일리 지출 상태 변환 성공 : GOOD")
     @Test
     void convertGOOD() {
         // 예산: 10000원, 기간: 10일 → 하루 예산: 10000
-        ExpenseStatus status = ExpenseStatus.convert(expenseSavingGoal, 8500);
+        ExpenseStatus status = ExpenseStatus.convert(10_000, 8500);
 
         Assertions.assertThat(status).isEqualTo(ExpenseStatus.GOOD);
     }
@@ -27,7 +18,7 @@ class ExpenseStatusTest {
     @DisplayName("데일리 지출 상태 변환 성공 : BAD")
     @Test
     void convertBAD() {
-        ExpenseStatus status = ExpenseStatus.convert(expenseSavingGoal, 11_000);
+        ExpenseStatus status = ExpenseStatus.convert(10_000, 11_000);
 
         Assertions.assertThat(status).isEqualTo(ExpenseStatus.BAD);
     }

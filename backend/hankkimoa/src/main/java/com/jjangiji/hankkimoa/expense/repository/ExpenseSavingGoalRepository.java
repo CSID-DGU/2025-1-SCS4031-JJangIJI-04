24e00bd9cf224ab49ab7eb40661f6c05a15e02b1
jinwoo22 <jenny010925@naver.com>
2025-04-29 20:13:06 +0900

    feat: 최신 지출 목표 금액 가져오는 기능 구현

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/repository/ExpenseSavingGoalRepository.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/expense/repository/ExpenseSavingGoalRepository.java
@@ -1,7 +1,14 @@
 package com.jjangiji.hankkimoa.expense.repository;
 
 import com.jjangiji.hankkimoa.expense.domain.ExpenseSavingGoal;
+import com.jjangiji.hankkimoa.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 public interface ExpenseSavingGoalRepository extends JpaRepository<ExpenseSavingGoal, Long> {
+
+    @Query("SELECT e FROM ExpenseSavingGoal e "
+            + "WHERE e.user = :user ORDER BY e.createdAt DESC limit 1 ")
+    Optional<ExpenseSavingGoal> findLastByUser(User user);
 }
---
+++ backend/hankkimoa/src/test/java/com/jjangiji/hankkimoa/expense/repository/ExpenseSavingGoalRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.jjangiji.hankkimoa.expense.repository;
+
+
+import com.jjangiji.hankkimoa.config.RepositoryTest;
+import com.jjangiji.hankkimoa.expense.domain.ExpenseSavingGoal;
+import com.jjangiji.hankkimoa.user.domain.User;
+import com.jjangiji.hankkimoa.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+import java.util.Optional;
+
+class ExpenseSavingGoalRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    private ExpenseSavingGoalRepository expenseSavingGoalRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("최신 목표 금액 조회")
+    @Test
+    void findLastByUser() {
+        // given
+        User user = new User("한끼", "hankki");
+        User savedUser = userRepository.save(user);
+
+        ExpenseSavingGoal expenseSavingGoal1 = expenseSavingGoalRepository.save(
+                new ExpenseSavingGoal(savedUser,100_000,
+                        LocalDate.of(2025, 4, 20),
+                        LocalDate.of(2025, 4, 26)));
+        ExpenseSavingGoal expenseSavingGoal2 = expenseSavingGoalRepository.save(
+                new ExpenseSavingGoal(savedUser,100_000,
+                        LocalDate.of(2025, 4, 27),
+                        LocalDate.of(2025, 5, 3)));
+
+        expenseSavingGoalRepository.save(expenseSavingGoal1);
+        expenseSavingGoalRepository.save(expenseSavingGoal2);
+
+        // when
+        Optional<ExpenseSavingGoal> result = expenseSavingGoalRepository.findLastByUser(user);
+
+        // then
+        Assertions.assertThat(result.get()).isEqualTo(expenseSavingGoal2);
+    }
+}

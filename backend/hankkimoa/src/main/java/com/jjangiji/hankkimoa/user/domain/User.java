89b578886942dc01c6ebb2115d927db9a9cedb37
jinwoo22 <jenny010925@naver.com>
2025-05-07 01:06:01 +0900

    feat: 유저 도메인 필드 추가

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/domain/User.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/domain/User.java
@@ -1,39 +1,60 @@
 package com.jjangiji.hankkimoa.user.domain;
 
+import com.jjangiji.hankkimoa.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor
 @Getter
 @Entity
 @Table(name="users")
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String nickname;
 
-    private String socialId;
+    @Column(nullable = false)
+    private String imageUrl;
 
-    public User(Long id, String nickname, String socialId) {
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LoginType loginType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private boolean isExpenseOpen = true;
+
+    public User(Long id, String email, String nickname) {
         this.id = id;
+        this.email = email;
         this.nickname = nickname;
-        this.socialId = socialId;
     }
 
-    public User(String nickname, String socialId) {
+    public User(String email, String nickname, String imageUrl, LoginType loginType, Role role) {
+        this.email = email;
         this.nickname = nickname;
-        this.socialId = socialId;
+        this.imageUrl = imageUrl;
+        this.loginType = loginType;
+        this.role = role;
     }
 }

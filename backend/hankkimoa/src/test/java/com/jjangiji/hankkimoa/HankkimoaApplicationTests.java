530d016cccd6300c39861353f164a621f771b9c0
jinwoo22 <jenny010925@naver.com>
2025-05-07 20:54:03 +0900

    fix: 테스트 파일 적용 안되는 오류 해결

---
+++ backend/hankkimoa/src/test/java/com/jjangiji/hankkimoa/HankkimoaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jjangiji.hankkimoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class HankkimoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

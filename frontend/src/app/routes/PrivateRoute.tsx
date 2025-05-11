edee531a3c855b937b80c37ba5caecb564f50a3a
이희주 <lhj2778@dongguk.edu>
2025-05-11 15:11:35 +0900

    feat: 토큰 리프레쉬 로직 수정

--- frontend/src/app/routes/PrivateRoute.tsx
+++ frontend/src/app/routes/PrivateRoute.tsx
@@ -6,6 +6,7 @@ import { useAuthStore } from '@/features/auth/store/useAuthStore';
 
 export const PrivateRoute = () => {
   const accessToken = useAuthStore((state) => state.accessToken);
+  const isInitializing = useAuthStore((state) => state.isInitializing);
   //const checkCompletion = useCheckUserCompletion();
 
   //로컬 테스트 시에는 주석 달고 진행
@@ -14,6 +15,10 @@ export const PrivateRoute = () => {
   //    checkCompletion();
   //  }
   //}, [accessToken, checkCompletion]);
+
+  if (isInitializing) {
+    return null; // 초기화 중에는 아무것도 렌더링하지 않음
+  }
 
   if (!accessToken) {
     return <Navigate to="/landing" replace />;

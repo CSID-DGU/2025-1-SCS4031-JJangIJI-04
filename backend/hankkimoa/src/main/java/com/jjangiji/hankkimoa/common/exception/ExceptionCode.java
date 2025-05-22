71eeaa75dd333b9daecbf301ce21474ad59c5302
JiKyoungmin <jenny1672@dgu.ac.kr>
2025-05-22 20:13:40 +0900

    Revert "[BE] 북마크 추가/제거/조회 API 추가 (#68)"
    This reverts commit bebaa6ba3117ab1f1abd79fc98b890e19f5f268f.

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/common/exception/ExceptionCode.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/common/exception/ExceptionCode.java
@@ -49,9 +49,6 @@ public enum ExceptionCode {
     AUTHENTICATION_TOKEN_USER_MISMATCH(HttpStatus.UNAUTHORIZED, "엑세스 토큰과 리프레시 토큰의 소유자가 다릅니다."),
     AUTHENTICATION_TOKEN_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "토큰 타입이 올바르지 않습니다."),
 
-    //북마크
-    BOOKMARK_EXISTS(HttpStatus.BAD_REQUEST, "즐겨찾기에 이미 추가된 식당입니다."),
-    BOOKMARK_NOT_FOUND(HttpStatus.BAD_REQUEST, "즐겨찾기를 찾을 수 없습니다."),
     ;
     private final HttpStatus httpStatus;
     private final String message;

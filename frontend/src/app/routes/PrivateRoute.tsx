38f5ca6741539aedb29bd916f51554725aae635c
이희주 <lhj2778@dongguk.edu>
2025-05-26 01:10:17 +0900

    #84 fix: 지출기록 누락 타입 추가

--- frontend/src/app/routes/PrivateRoute.tsx
+++ frontend/src/app/routes/PrivateRoute.tsx
@@ -11,8 +11,8 @@ export const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const isInitializing = useAuthStore((state) => state.isInitializing);
 
   //로컬에서 UI 테스트 할 때 return 부분 주석 처리
-  //if (isInitializing) return null;
-  //if (!nickname) return <Navigate to="/landing" replace />;
+  if (isInitializing) return null;
+  if (!nickname) return <Navigate to="/landing" replace />;
   return children;
 };
 

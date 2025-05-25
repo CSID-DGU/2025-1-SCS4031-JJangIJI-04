10744abb0352c072b6b59fbde36ce9e6976a00ff
이희주 <lhj2778@dongguk.edu>
2025-05-25 19:08:51 +0900

    fix: 가용 금액 조회 API URI 변경

--- frontend/src/features/goals/api/requestGetRemainingBudget.ts
+++ frontend/src/features/goals/api/requestGetRemainingBudget.ts
@@ -3,6 +3,8 @@ import { format } from 'date-fns';
 
 export const requestGetRemainingBudget = async (): Promise<number> => {
   const today = format(new Date(), 'yyyy-MM-dd');
-  const res = await api.get(`/saving-goals?date=${today}/remaining`);
+  const res = await api.get('/saving-goals/remaining', {
+    params: { date: today }
+  });
   return res.data.remainingBudget;
 };

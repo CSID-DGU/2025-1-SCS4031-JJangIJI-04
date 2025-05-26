7bae8eca16bb11b9dbd565e257f5aaa92ca5b7fe
jinwoo22 <jenny010925@naver.com>
2025-05-26 22:24:12 +0900

    feat: 식당 유니크 아이디 가져오는 기능 추가

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/restaurant/repository/RestaurantRepository.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/restaurant/repository/RestaurantRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
+import java.util.Set;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     @Query(value = "SELECT * FROM restaurant r WHERE MATCH(name) AGAINST(:word IN NATURAL LANGUAGE MODE)",
             nativeQuery = true)
     List<Restaurant> findAllByWord(@Param("word") String word);
+
+    @Query("SELECT r.uniqueId FROM Restaurant r")
+    Set<String> findAllUniqueId();
 }

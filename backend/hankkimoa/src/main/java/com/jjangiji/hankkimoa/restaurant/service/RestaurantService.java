82109249682e29f1a4ed42463750d0e45da51299
jinwoo22 <jenny010925@naver.com>
2025-05-26 22:24:28 +0900

    refactor: 중복 데이터 제외하고 식당 추가하도록 변경

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/restaurant/service/RestaurantService.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/restaurant/service/RestaurantService.java
@@ -1,11 +1,11 @@
 package com.jjangiji.hankkimoa.restaurant.service;
 
+import com.jjangiji.hankkimoa.restaurant.domain.Address;
 import com.jjangiji.hankkimoa.restaurant.domain.Category;
 import com.jjangiji.hankkimoa.restaurant.domain.CategoryMapper;
 import com.jjangiji.hankkimoa.restaurant.domain.Menu;
 import com.jjangiji.hankkimoa.restaurant.domain.OpeningHours;
 import com.jjangiji.hankkimoa.restaurant.domain.Restaurant;
-import com.jjangiji.hankkimoa.restaurant.domain.Address;
 import com.jjangiji.hankkimoa.restaurant.domain.RestaurantImage;
 import com.jjangiji.hankkimoa.restaurant.repository.CategoryRepository;
 import com.jjangiji.hankkimoa.restaurant.repository.MenuRepository;
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -31,12 +32,12 @@ public class RestaurantService {
 
     @Transactional
     public void createRestaurants(List<RestaurantCreateRequest> requests) {
-        restaurantRepository.deleteAll(); // todo 리팩토링 ⚒️
-
+        Set<String> uniqueIds = restaurantRepository.findAllUniqueId();
         CategoryMapper categoryMapper = new CategoryMapper(categoryRepository.findAll());
 
         for (RestaurantCreateRequest request : requests) {
             if (request.menus() == null) continue;
+            if (request.id() == null || uniqueIds.contains(request.id())) continue;
 
             Restaurant restaurant = saveRestaurant(request, categoryMapper);
             saveRestaurantImages(restaurant, request);
@@ -54,6 +55,8 @@ public class RestaurantService {
         Restaurant restaurant = new Restaurant(category, request.name(), request.id(), request.menu_average(), address);
         return restaurantRepository.save(restaurant);
     }
+
+
 
     private void saveRestaurantImages(Restaurant restaurant, RestaurantCreateRequest request) {
         if (request.images() == null) return;

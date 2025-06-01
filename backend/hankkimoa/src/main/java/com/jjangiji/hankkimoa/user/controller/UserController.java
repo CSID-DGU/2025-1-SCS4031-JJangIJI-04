0d04f5272a2f315953ffcfa5cd388a7e2c3638c9
jinwoo22 <jenny010925@naver.com>
2025-06-01 19:19:25 +0900

    feat: 사용자 카테고리 수정 기능 구현

--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/controller/UserController.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/controller/UserController.java
@@ -1,25 +1,32 @@
-package com.jjangiji.hankkimoa.user;
+package com.jjangiji.hankkimoa.user.controller;
 
 import com.jjangiji.hankkimoa.auth.config.AuthRequiredPrincipal;
-import com.jjangiji.hankkimoa.auth.controller.cookie.CookieResolver;
 import com.jjangiji.hankkimoa.user.domain.User;
 import com.jjangiji.hankkimoa.user.service.UserService;
+import com.jjangiji.hankkimoa.user.service.dto.CategoryUpdateRequest;
 import com.jjangiji.hankkimoa.user.service.dto.UserMeResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
+
     private final UserService userService;
-    private final CookieResolver cookieResolver;
 
     @GetMapping("/api/users/me")
-    public ResponseEntity<UserMeResponse> getMyInfo(@AuthRequiredPrincipal User user, HttpServletRequest request) {
-        UserMeResponse response = userService.getMyInfo(user);
+    public ResponseEntity<UserMeResponse> readMyInfo(@AuthRequiredPrincipal User user, HttpServletRequest request) {
+        UserMeResponse response = userService.readMyInfo(user);
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/api/categories")
+    public ResponseEntity<Void> updateCategories(@AuthRequiredPrincipal User user, CategoryUpdateRequest request) {
+        userService.updateCategories(user, request);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/repository/UserCategoryRepository.java
+++ backend/hankkimoa/src/main/java/com/jjangiji/hankkimoa/user/repository/UserCategoryRepository.java
@@ -3,10 +3,9 @@ package com.jjangiji.hankkimoa.user.repository;
 import com.jjangiji.hankkimoa.user.domain.User;
 import com.jjangiji.hankkimoa.user.domain.UserCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface UserCategoryRepository extends JpaRepository<UserCategory, Long> {
     List<UserCategory> findAllByUser(User user);
+    void deleteAllByUserId(Long userId);
 }

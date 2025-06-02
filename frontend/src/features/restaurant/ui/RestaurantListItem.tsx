c592b9eac69445d9de5c1416e66dbc7b3c6e0926
hjlee2778 <lhj2778@dongguk.edu>
2025-06-02 22:37:07 +0900

    fix: 북마크 쿼리 관련 로직 수정

--- frontend/src/features/restaurant/ui/RestaurantListItem.tsx
+++ frontend/src/features/restaurant/ui/RestaurantListItem.tsx
@@ -4,6 +4,7 @@ import { Crown } from '@/features/restaurant/ui/crown/Crown';
 import { BookmarkButton } from '@/features/restaurant/ui/bookmark/BookmarkButton';
 import { IconTextRow } from '@/features/restaurant/ui/IconTextRow';
 import { useToggleBookmark } from '@/features/restaurant/mutations/useToggleBookmark';
+import { useState } from 'react';
 
 interface RestaurantItem {
   id: number;
